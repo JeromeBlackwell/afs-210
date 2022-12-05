76400ca24bbd33ee4352366f2f8739ed796545e8
JeromeBlackwell <71746521+JeromeBlackwell@users.noreply.github.com>
2022-12-05 03:19:26 -0500

    week1

--- week1/payroll.py
+++ week1/payroll.py
@@ -1,18 +1,26 @@
 class Employee:
-    def_init_(self, employeId, firstName, lastName, hourlyPayRate, hoursWorked, WeeklyPay):
+    def _init_(self, employeeId, firstName, lastName, hourlyPayRate, hoursWorked, WeeklyPay):
+        firstName = str(input("Enter your First Name:"))
+        lastName = str(input("Enter your Last Name:"))
+        employeeId = int(input("Enter your Employee ID:"))
+        hourlyPayRate = float(input("Enter your Hourly Pay Rate:"))
+        hoursWorked = float(input("Enter total hours worked for the week:"))
+        weeklyPay = float()
 
-    firstName = str(input("Enter your First Name:"))
-    lastName = str(input("Enter your Last Name:"))
-    employeeId = int(input("Enter your Employee ID:"))
-    hourlyPayRate = float(input("Enter your Hourly Pay Rate:"))
-    hoursWorked = float(input("Enter total hours worked for the week:"))
-    weeklyPay = float()
+    def showPay():   
+        firstName = ""
+        lastName = ""
+        employeeId = 0
+        hourlyPayRate = 0.00
+        hoursWorked = 0
+        weeklyPay = 0.00
+        hoursWorked <= 40 (hoursWorked * hourlyPayRate)
+        if hoursWorked >40 (hoursWorked >40(hourlyPayRate * 1.5 * hoursWorked)):
+           
 
-
-
-    print("Employee ID: ",employeId)
-    print("First Name: ",firstName)
-    print("Last Name: ", lastName)
-    print("Hourly Rate: ", hourlyPayRate)
-    print("Total Hours Worked: ", hoursWorked)
-    print("Total Pay Earned: ", weeklyPay)
+            print("Employee ID: ",employeeId)
+            print("First Name: ",firstName)
+            print("Last Name: ", lastName)
+            print("Hourly Rate: ", hourlyPayRate)
+            print("Total Hours Worked: ", hoursWorked)
+            print("Total Pay Earned: ", weeklyPay)
