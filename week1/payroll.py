8f1f076023b4023b6c5943f17428925419cd6cfb
JeromeBlackwell <71746521+JeromeBlackwell@users.noreply.github.com>
2022-12-04 19:23:48 -0500

    week1

---
+++ week1/payroll.py
@@ -0,0 +1,18 @@
+class Employee:
+    def_init_(self, employeId, firstName, lastName, hourlyPayRate, hoursWorked, WeeklyPay):
+
+    firstName = str(input("Enter your First Name:"))
+    lastName = str(input("Enter your Last Name:"))
+    employeeId = int(input("Enter your Employee ID:"))
+    hourlyPayRate = float(input("Enter your Hourly Pay Rate:"))
+    hoursWorked = float(input("Enter total hours worked for the week:"))
+    weeklyPay = float()
+
+
+
+    print("Employee ID: ",employeId)
+    print("First Name: ",firstName)
+    print("Last Name: ", lastName)
+    print("Hourly Rate: ", hourlyPayRate)
+    print("Total Hours Worked: ", hoursWorked)
+    print("Total Pay Earned: ", weeklyPay)
