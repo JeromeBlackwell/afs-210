c9dbc33667fc1b606ab9bff3649ed71fa66d1549
JeromeBlackwell <71746521+JeromeBlackwell@users.noreply.github.com>
2022-12-18 17:32:38 -0500

    week2

--- week2/recursion.py
+++ week2/recursion.py
@@ -1,40 +1,40 @@
 
 def loop1():
-# #     # Sum the odd numbers between 1 and 20
+    # Sum the odd numbers between 1 and 20
     odd_sum = 0
-    for i in range(0,20):
+    for i in range(20):
         if (i % 2) == 1:
             odd_sum += i
-        # i += 1
-    return odd_sum
+        return odd_sum
 
 def loop2():
-    # Sum the even numbers between 1 and 20
+#     # Sum the even numbers between 1 and 20
+    i = 0
     even_sum = 0
-    for i in range(0,20):
+    while i < 20:
         if (i % 2) == 0:
             even_sum += i
-        # i += 1
+        i += 1
     return even_sum
 
 def loop1Rec(num,odd_sum):
-# #     Duplicate the loop1 function using recursion
+#  #   Duplicate the loop1 function using recursion
     if (num >=20):
         return odd_sum
-    # elif (num % 2) ==1:
-    odd_sum += num
+    elif (num % 2) ==1:
+            odd_sum += num
     return loop1Rec(num+2,odd_sum)
-
-print(f"Sum of odd numbers between 1 and 20 using a for loop = {loop1()}")
-print(f"Sum of odd numbers between 1 and 20 using recursion = {loop1Rec(3,1)}")
+        
+print(f"The sum of odd numbers between 1 and 20 using a for loop is {loop1()}")
+print(f"The sum of odd numbers between 1 and 20 using the recursion method is {loop1Rec(3,1)}")
 
 def loop2Rec(num,even_sum):
-    # Duplicate the loop2 function using recursion
+#      # Duplicate the loop2 function using recursion
     if (num >=20):
-        return even_sum
-    # elif (num % 2) ==0:
-    even_sum += num
-    return loop2Rec(num+2,even_sum)
-
-print(f"Sum of even numbers between 1 and 20 using a for loop = {loop2()}")
-print(f"Sum of even numbers between 1 and 20 using recursion = {loop2Rec(2,0)}")
+        return even_sum 
+    elif (num % 2) ==0:
+        even_sum += num
+        return loop2Rec(num+2,even_sum)
+    
+print(f"The sum of even numbers between 1 and 20 using a for 'while loop' is {loop2()}")
+print(f"The sum of even numbers between 1 and 20 using the recursion method is {loop2Rec(2,0)}")
