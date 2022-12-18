84f71132e112aa736677cd7cc85283de39da6e7e
JeromeBlackwell <71746521+JeromeBlackwell@users.noreply.github.com>
2022-12-18 00:26:24 -0500

    week2

--- week2/recursion.py
+++ week2/recursion.py
@@ -5,16 +5,17 @@ def loop1():
     for i in range(0,20):
         if (i % 2) == 1:
             odd_sum += i
+        # i += 1
     return odd_sum
 
-# def loop2():
-#     # Sum the even numbers between 1 and 20
-#     even_sum = 0
-#     for i in range(20):
-#         if (i % 2) == 0:
-#             even_sum += i
-#         i += 1
-#     return even_sum
+def loop2():
+    # Sum the even numbers between 1 and 20
+    even_sum = 0
+    for i in range(0,20):
+        if (i % 2) == 0:
+            even_sum += i
+        # i += 1
+    return even_sum
 
 def loop1Rec(num,odd_sum):
 # #     Duplicate the loop1 function using recursion
@@ -27,14 +28,14 @@ def loop1Rec(num,odd_sum):
 print(f"Sum of odd numbers between 1 and 20 using a for loop = {loop1()}")
 print(f"Sum of odd numbers between 1 and 20 using recursion = {loop1Rec(3,1)}")
 
-# def loop2Rec(num,even_sum):
+def loop2Rec(num,even_sum):
     # Duplicate the loop2 function using recursion
 
-#     if (num >=20):
-#         return even_sum
-#     elif (num % 2) ==1:
-#             even_sum += num
-#     return loop2Rec(num+3,even_sum)
+    if (num >=20):
+        return even_sum
+    # elif (num % 2) ==0:
+    even_sum += num
+    return loop2Rec(num+2,even_sum)
 
-# print(f"Sum of odd numbers between 1 and 20 using a for loop= {loop1()}")
-# print(f"Sum of odd numbers between 1 and 20 using recursion= {loop1Rec(0,0)}")
+print(f"Sum of even numbers between 1 and 20 using a for loop = {loop2()}")
+print(f"Sum of even numbers between 1 and 20 using recursion = {loop2Rec(2,0)}")
