74216b219e004ff9b19f37a5f7cb4e44c1a4ecec
JeromeBlackwell <71746521+JeromeBlackwell@users.noreply.github.com>
2023-01-04 13:07:40 -0500

    week5

--- week5/searching/binarysearch.py
+++ week5/searching/binarysearch.py
@@ -1,14 +1,21 @@
 def binary_search(list, item):
-    list_size = len(list)
-    for i in range(list_size):
-        if item == list[i]:
+    low = 0
+    high = len(list) -1
+    while high >= low:
+        mid = (high + low) // 2
+        if list[mid] == item:
             return True
-        elif list[i] > item:
-            return False
-        
+        elif list[mid] < item:
+            low = mid + 1
+        else: 
+            high = mid -1   
+    else:
+        return False
+            
 list = [7, 20, 26, 31, 40, 51, 55, 63, 74, 81]
 list2 = [7, 20, 26, 31, 40, 51, 55, 63, 74, 81]
 list3 = ["Alpha", "Beta", "Delta","Epsilon", "Gamma", "Theta", "Zeta"]
+
 
 print(binary_search(list,31))
 print(binary_search(list,77))
