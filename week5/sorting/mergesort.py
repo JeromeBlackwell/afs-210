43054ae636d407ed5f68fbebf2b4ea32338a3c57
JeromeBlackwell <71746521+JeromeBlackwell@users.noreply.github.com>
2023-01-07 12:54:18 -0500

    week5

--- week5/sorting/mergesort.py
+++ week5/sorting/mergesort.py
@@ -1,6 +1,24 @@
 def mergeSort(nlist):
     print("Splitting ",nlist)
     # insert your code to complete the mergeSort function
+    
+    if len(nlist) >1:
+        mid = len(nlist) // 2
+        left = nlist[:mid]
+        right = nlist[mid:]
+    
+    #Recursive call on each half
+        mergeSort(left)
+        mergeSort(right)
+    
+    #Two iterators traversing the halvew
+        i = 0
+        j = 0
+    
+    #iterator main list
+        K = 0
+    
+    
     print("Merging ",nlist)
 
 def merge(nlist,lefthalf,righthalf):
@@ -25,3 +43,4 @@ def merge(nlist,lefthalf,righthalf):
         k=k+1
     return nlist
 
+nlist = [55, 31, 26, 20, 63, 7, 51, 74, 81, 40]
